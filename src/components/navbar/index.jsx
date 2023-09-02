1e8bd74aa123cd62fb0916314d8fd6d1fc68ba84
unknown <harshal011kamble@gmail.com>
2023-09-02 12:03:24 +0530

    finised almost

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,16 @@
 import "./styles.css";
-// import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
-// import { useState } from "react";
+import { GiHamburgerMenu, GiCancel } from "react-icons/gi";
+import { useState } from "react";
 let Navbar = () => {
-    // let [navToggle, setNavToggle] = useState(false);
+    let [navToggle, setNavToggle] = useState(false);
     return (
         <section id="Navbar">
             <div className="navbar flex flex-row gap-x-8 justify-between">
                 <div className="flex flex-row">
                     <p>harshal</p>
                 </div>
-                {/* <GiHamburgerMenu size={"1.5rem"} className={navToggle ? " invincible " : " self-center hamburger "} onClick={() => setNavToggle(!navToggle)} />
-                <GiCancel size={"1.5rem"} className={navToggle ? "self-center cancel" : "invincible"} onClick={() => setNavToggle(!navToggle)} /> */}
+                <GiHamburgerMenu size={"1.5rem"} className={navToggle ? " invincible " : " self-center hamburger "} onClick={() => setNavToggle(!navToggle)} />
+                <GiCancel size={"1.5rem"} className={navToggle ? "self-center cancel" : "invincible"} onClick={() => setNavToggle(!navToggle)} />
 
                 <div className="gap-x-8 top-nav-bar-items">
                     <a href="#Home"><p>home</p></a>
@@ -19,12 +19,12 @@ let Navbar = () => {
                     <a href="#About"><p>about</p></a>
                 </div>
             </div>
-            {/* <div className={navToggle ? "toggle-dropdown" : " invincible"}>
+            <div className={navToggle ? "toggle-dropdown" : " invincible"}>
                 <a href="#Home"><p className="py-1">home</p></a>                
                 <a href="#Skills"><p className="py-1">skills</p></a>               
                 <a href="#Project"><p className="py-1">projects</p></a>
                 <a href="About"><p className="py-1">about</p></a>              
-            </div> */}
+            </div>
         </section>
     )
 }
