146f75d6ba5eb17ef8f550dfef5a3f816a584062
unknown <harshal011kamble@gmail.com>
2023-09-02 11:31:47 +0530

    resolving minor issues

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -9,22 +9,22 @@ let Navbar = () => {
                 <div className="flex flex-row">
                     <p>harshal</p>
                 </div>
-                <GiHamburgerMenu size={"1.5rem"} className={navToggle ? " invincible " : " self-center hamburger "} onClick={() => setNavToggle(!navToggle)} />
-                <GiCancel size={"1.5rem"} className={navToggle ? "self-center cancel" : "invincible"} onClick={() => setNavToggle(!navToggle)} />
+                {/* <GiHamburgerMenu size={"1.5rem"} className={navToggle ? " invincible " : " self-center hamburger "} onClick={() => setNavToggle(!navToggle)} />
+                <GiCancel size={"1.5rem"} className={navToggle ? "self-center cancel" : "invincible"} onClick={() => setNavToggle(!navToggle)} /> */}
 
-                <div className="flex flex-row justify-center gap-x-8 navbar-items">
+                <div className="flex flex-row justify-center gap-x-8 navu-items">
                     <a href="#Home"><p>home</p></a>
                     <a href="#Skills"><p>skills</p></a>
                     <a href="#Project"><p>projects</p></a>
                     <a href="#About"><p>about</p></a>
                 </div>
             </div>
-            <div className={navToggle ? "toggle-dropdown" : " invincible"}>
+            {/* <div className={navToggle ? "toggle-dropdown" : " invincible"}>
                 <a href="#Home"><p className="py-1">home</p></a>                
                 <a href="#Skills"><p className="py-1">skills</p></a>               
                 <a href="#Project"><p className="py-1">projects</p></a>
                 <a href="About"><p className="py-1">about</p></a>              
-            </div>
+            </div> */}
         </section>
     )
 }
