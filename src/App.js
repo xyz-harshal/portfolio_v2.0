c6598fdfb48388e59ba9e8dfe7d94e3896b58775
unknown <harshal011kamble@gmail.com>
2023-09-02 21:56:30 +0530

    done applying finsihing touch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import './App.css';
 import Navbar from "./components/navbar";
 import Home from './components/home';
 import Skills from './components/skills';
 import Project from './components/project';
 import About from './components/about';
 import Fotter from './components/footer';
-function App() {
+export default function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <Home />
       <Skills />
       <Project />
       <About />
       <Fotter />
-      </div>
+    </>
   );
 }
-
-export default App;
